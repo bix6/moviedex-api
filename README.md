af345262313f2fed46acb95731c4810523d36fc7
bix <840573+bix6@users.noreply.github.com>
2020-03-31 11:25:32 -0700

    complete. add cors, helmet

---
+++ README.md
@@ -0,0 +1,8 @@
+# Moviedex API
+
+## Express Practice
+
+### Sample Call
+- Endpoint: `GET /movies`
+- Query params: `genre`, `country`, `avg_vote`
+- Example: `http://localhost:8000/movie?genre=Comedy&country=United&avg_vote=7`
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const MOVIES = require('./movies.json');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: "Unauthorized Request" });
+    }
+
+    next();
+});
 
 function handleGetMovie(req, res) {
     // Init response with all movies
